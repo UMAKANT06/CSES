94427eb19e2dc4580e56cd2fe9a22203e7eb17a7
UMAKANT06 <sahuumakant877@gmail.com>
2024-08-16 10:42:22 +0530

    done

--- flight_route.cpp
+++ flight_route.cpp
@@ -1,48 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void dfs(long long s,vector<bool>&visited,vector<vector<long long>>&adj,vector<int>&trav){
-    visited[s]=true;
-    trav.push_back(s);
-    for(auto u:adj[s]){
-        if(!visited[u]){
-            dfs(u,visited,adj,trav);
+
+void dfs(long long s, vector<bool>& visited, vector<vector<long long>>& adj) {
+    visited[s] = true;
+    for (auto u : adj[s]) {
+        if (!visited[u]) {
+            dfs(u, visited, adj);
         }
     }
 }
-int main(){
-    long long n,m;
-    cin>>n>>m;
-    vector<vector<long long>>adj(n+1);
-    set<int>k;
-    for(long long i=0;i<m;i++){
-        long long a,b;
-        cin>>a>>b;
+
+int main() {
+    long long n, m;
+    cin >> n >> m;
+    vector<vector<long long>> adj(n + 1), adj_rev(n + 1);
+
+    for (long long i = 0; i < m; i++) {
+        long long a, b;
+        cin >> a >> b;
         adj[a].push_back(b);
-        k.insert(a);
+        adj_rev[b].push_back(a);  // Reverse graph
     }
 
+    vector<bool> visited(n + 1, false);
 
-
-    bool isConnected=true;
-    for (long long j=1;j<=k.size();j++){
-        vector<int>trav;
-        vector<bool>visited(n+1,false);
-        if(!visited[j]){
-            dfs(j,visited,adj,trav);
+    // Step 1: DFS on original graph
+    dfs(1, visited, adj);
+    for (long long i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            cout << "NO" << endl;
+            cout << 1 << " " << i << endl;
+            return 0;
         }
-
-        
-            if(trav.size()!=n){
-                isConnected=false;
-                break;
-            }
-        
     }
 
-    if(isConnected==true){
-        cout<<"YES"<<endl;
-    }else{
-        cout<<"NO"<<endl;
+    // Step 2: DFS on reversed graph
+    fill(visited.begin(), visited.end(), false);
+    dfs(1, visited, adj_rev);
+    for (long long i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            cout << "NO" << endl;
+            cout << i << " " << 1 << endl;
+            return 0;
+        }
     }
+
+    // If both DFS traversals visit all cities, the graph is strongly connected
+    cout << "YES" << endl;
+
     return 0;
 }
